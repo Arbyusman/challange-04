599394d155eb932f758b20d1902d1a3cec977515
arby usman <91175559+Arbyusman@users.noreply.github.com>
2022-09-23 22:50:34 +0800

    add: HTTP status

--- server/index.js
+++ server/index.js
@@ -4,26 +4,29 @@ const port = 2424;
 const path = require("path");
 const public = path.join(__dirname, "../public");
 
+// load public directori
 app.use(express.static(public));
 
 app.get("/", function (req, res) {
-  res.set('Content-Type', 'text/html')
-  res.sendFile(path.join(public, "landing_page.html"));
-  res.status(200);
-
+  res
+    .status(200)
+    .set("Content-Type", "text/html")
+    .sendFile(path.join(public, "landing_page.html"));
 });
+
 app.get("/cars", function (req, res) {
-  res.set('Content-Type', 'text/html')
-  res.sendFile(path.join(public, "cari_mobil.html"));
-  res.status(200);
+  res
+    .status(200)
+    .set("Content-Type", "text/html")
+    .sendFile(path.join(public, "cari_mobil.html"));
 });
 
 app.get("*", function (req, res) {
-  res.set('Content-Type', 'text/html')
-  res.sendFile(path.join(public, "404.html"));
-  res.status(404);
+  res
+    .status(404)
+    .set("Content-Type", "text/html")
+    .sendFile(path.join(public, "404.html"));
 });
-
 
 app.listen(port, () => {
   console.log("Example app listening on http://localhost:%d", port);
